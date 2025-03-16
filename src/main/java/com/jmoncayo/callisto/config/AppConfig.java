a19740826b3b310fd2dcff3cd5df121753484b35
andresmonc <jaimeamonc@gmail.com>
2025-03-16 15:07:29 -0500

    comments

--- src/main/java/com/jmoncayo/callisto/config/AppConfig.java
+++ src/main/java/com/jmoncayo/callisto/config/AppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "com.jmoncayo.callisto") // Replace with your package
+@ComponentScan(basePackages = "com.jmoncayo.callisto")
 public class AppConfig {
 
     @Bean
