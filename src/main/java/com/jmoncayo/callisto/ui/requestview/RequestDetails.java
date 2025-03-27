ee73e14b0909855faf071c84a2c9f77584ba44e0
andresmonc <jaimeamonc@gmail.com>
2025-03-26 19:54:48 -0500

    what a stupid bug

--- src/main/java/com/jmoncayo/callisto/ui/requestview/RequestDetails.java
+++ src/main/java/com/jmoncayo/callisto/ui/requestview/RequestDetails.java
@@ -1,14 +1,16 @@
 package com.jmoncayo.callisto.ui.requestview;
 
-import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
-
+import com.jmoncayo.callisto.ui.requestview.tabs.AuthorizationTab;
+import com.jmoncayo.callisto.ui.requestview.tabs.BodyTab;
 import com.jmoncayo.callisto.ui.requestview.tabs.HeadersTab;
-import java.util.List;
-import javafx.scene.control.Tab;
+import com.jmoncayo.callisto.ui.requestview.tabs.ParamsTab;
+import com.jmoncayo.callisto.ui.requestview.tabs.SettingsTab;
 import javafx.scene.control.TabPane;
 import lombok.Getter;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
 
 @Component
 @Scope(SCOPE_PROTOTYPE)
@@ -16,8 +18,8 @@ public class RequestDetails extends TabPane {
 	@Getter
 	private final HeadersTab headersTab;
 
-	public RequestDetails(List<Tab> tabs, HeadersTab headersTab) {
+	public RequestDetails(ParamsTab paramsTab, AuthorizationTab authorizationTab, HeadersTab headersTab, BodyTab bodyTab, SettingsTab settingsTab) {
 		this.headersTab = headersTab;
-		this.getTabs().addAll(tabs);
+		this.getTabs().addAll(paramsTab, authorizationTab, headersTab, bodyTab, settingsTab);
 	}
 }
