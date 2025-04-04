93db7c744b3fcc80d3ac69afc09ba71694fb224b
andresmonc <jaimeamonc@gmail.com>
2025-04-04 15:13:12 -0500

    view collections on side nav

--- src/main/java/com/jmoncayo/callisto/ui/sidenavigation/SideNavigationCollectionAccordion.java
+++ src/main/java/com/jmoncayo/callisto/ui/sidenavigation/SideNavigationCollectionAccordion.java
@@ -4,10 +4,15 @@ import com.jmoncayo.callisto.collection.Collection;
 import com.jmoncayo.callisto.ui.controllers.CollectionController;
 import java.util.ArrayList;
 import java.util.List;
+
+import javafx.geometry.Insets;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TitledPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,18 +20,35 @@ public class SideNavigationCollectionAccordion extends Accordion {
 
 	public SideNavigationCollectionAccordion(CollectionController collectionController) {
 		List<Collection> collections = collectionController.getAllCollections();
-		List<TitledPane> panes = new ArrayList<>(collections.size());
 		for (Collection collection : collections) {
-			TitledPane collectionPane = new TitledPane(collection.getName(), new StackPane());
-			collectionPane.setContent(new StackPane());
-			panes.add(collectionPane);
+			VBox contentBox = new VBox(8); // spacing between subfolder items
+			buildSubfolderTree(contentBox, collection.getSubfolders(), 0);
+
+			TitledPane collectionPane = new TitledPane(collection.getName(), contentBox);
+			this.getPanes().add(collectionPane);
 		}
-		this.getPanes().addAll(panes);
+	}
+
+	private void buildSubfolderTree(VBox parentBox, List<Collection> subfolders, int depth) {
+		for (Collection subfolder : subfolders) {
+			HBox row = new HBox(4);
+			row.setPadding(new Insets(0, 0, 0, depth * 15)); // indent based on depth
+			Label label = new Label(subfolder.getName());
+			row.getChildren().add(label);
+
+			parentBox.getChildren().add(row);
+
+			// Recursively build children
+			buildSubfolderTree(parentBox, subfolder.getSubfolders(), depth + 1);
+		}
 	}
 
 	public void addNewCollection(Collection collection) {
-		TitledPane pane = new TitledPane(collection.getName(), new StackPane());
-		pane.setContent(new StackPane(new Button("Test")));
+		VBox contentBox = new VBox(8);
+		buildSubfolderTree(contentBox, collection.getSubfolders(), 0);
+
+		TitledPane pane = new TitledPane(collection.getName(), contentBox);
 		this.getPanes().add(pane);
 	}
 }
+
