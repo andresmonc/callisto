fc6f68463c2dad5ea207740d4ab1f07d90c6238b
andresmonc <jaimeamonc@gmail.com>
2025-05-08 14:38:30 -0500

    cool

--- src/main/java/com/jmoncayo/callisto/ui/environmentview/EnvironmentTab.java
+++ src/main/java/com/jmoncayo/callisto/ui/environmentview/EnvironmentTab.java
@@ -3,6 +3,8 @@ package com.jmoncayo.callisto.ui.environmentview;
 import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
 
 import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -10,11 +12,66 @@ import org.springframework.stereotype.Component;
 @Scope(SCOPE_PROTOTYPE)
 public class EnvironmentTab extends Tab {
 
+	private final TextField editableEnvironmentName;
+
 	public EnvironmentTab() {
+		StackPane stackPane = new StackPane();
+		editableEnvironmentName = new TextField();
+		editableEnvironmentName.setText("New Environment");
+
+		// Initially not editable and styled like a label
+		setTextFieldStyle(false);
+
+		editableEnvironmentName.setEditable(false);
+		editableEnvironmentName.setFocusTraversable(false);
+
+		// when name changes update tab name
+		editableEnvironmentName.textProperty().addListener((obs, oldText, newText) -> {
+			this.setText(newText);
+		});
+
+		editableEnvironmentName.setOnMouseClicked(event -> {
+			if (event.getClickCount() == 2) {
+				editableEnvironmentName.setEditable(true);
+				editableEnvironmentName.setFocusTraversable(true);
+				setTextFieldStyle(true);
+				editableEnvironmentName.requestFocus();
+				editableEnvironmentName.selectAll();
+			}
+		});
+
+		// When focus is lost, revert to non-editable label style
+		editableEnvironmentName.focusedProperty().addListener((obs, wasFocused, isNowFocused) -> {
+			if (!isNowFocused) {
+				editableEnvironmentName.setEditable(false);
+				editableEnvironmentName.setFocusTraversable(false);
+				setTextFieldStyle(false);
+			}
+		});
+
+		stackPane.getChildren().add(editableEnvironmentName);
 		this.setClosable(true);
+		this.setContent(stackPane);
 	}
 
 	public void initNewEnvironment() {
 		this.setText("New Environment");
+		this.editableEnvironmentName.setText("New Environment");
+	}
+
+	private void setTextFieldStyle(boolean editing) {
+		if (editing) {
+			editableEnvironmentName.setStyle("""
+            -fx-background-color: white;
+            -fx-border-color: lightgray;
+            -fx-padding: 2 4 2 4;
+        """);
+		} else {
+			editableEnvironmentName.setStyle("""
+            -fx-background-color: transparent;
+            -fx-border-color: transparent;
+            -fx-padding: 2 4 2 4;
+        """);
+		}
 	}
 }
